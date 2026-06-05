2264a5dc06333c2556bbdabf610b3328d31aac61
jonamuswele <jonathankaleme@gmail.com>
2026-06-05 15:08:43 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -91,7 +91,7 @@ export default function Home({ onNavigate }) {
         }
         .hero-img.visible {
           opacity: 1;
-          transform: scale(1.00);
+          transform: scale(0.95);
         }
 
         .slide-dot {
