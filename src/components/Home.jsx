951efdf308a6e67039ca29a8c95f9f9842c6bf59
jonamuswele <jonathankaleme@gmail.com>
2026-06-05 15:00:06 +0100

    Increase hero image scale from 0.5 to 0.8

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -91,7 +91,7 @@ export default function Home({ onNavigate }) {
         }
         .hero-img.visible {
           opacity: 1;
-          transform: scale(0.5);
+          transform: scale(0.8);
         }
 
         .slide-dot {
